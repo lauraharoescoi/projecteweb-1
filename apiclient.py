7a3ba5a5f55507bf06561110272c2328a19a344a
lauraharoescoi <lauraha.escoi@gmail.com>
2023-02-20 10:31:05 +0000

    webscrape i apiclient

---
+++ apiclient.py
@@ -0,0 +1,49 @@
+import urllib.request
+import xmltodict
+import pprint
+
+
+class ArxivClient(object):
+    def __init__(self) -> None:
+        self.url = "http://export.arxiv.org/api/query?start=0&max_results=10&search_query="
+        pass
+    
+    def query_arxiv(self, query):
+        url_query = urllib.request.urlopen(self.url+query)
+        xml_data = url_query.read().decode("utf-8")
+        return xml_data
+    
+    def convert_xml(self, results):
+        dades = xmltodict.parse(results)   
+        return dades
+    
+    def extract_data(slf, data):
+        results = []
+        print(data)
+        print(type(data))
+        
+        for e in data['feed']['entry']:
+            title = e['title']
+            if len(e['author']) > 1:
+                authors = [a['name'] for a in e['author']]
+                authors = ",".join(authors)
+            else:
+                authors = e['author']['name']
+            
+            results.append((title, authors))
+        return results
+    
+    def get_data(self, query):
+        # consultar API arxiu
+        result_arxiv = self.query_arxiv(query)
+        # convertir xml -> dades
+        dades = self.convert_xml(result_arxiv)
+        # reduir dades
+        resultats = self.extract_data(dades)
+        return resultats
+    
+        
+if __name__== "__main__":
+    client = ArxivClient()
+    dades = client.get_data('transformers')
+    pprint.pprint(dades)
---
+++ webscrape.py
@@ -0,0 +1,49 @@
+
+import urllib3
+import bs4
+
+class WebScrape(object):
+    
+    def __init__(self) -> None:
+        self.url = "https://www.99-bottles-of-beer.net/language-python-808.html"
+        
+    
+    def get_web(self):
+        # connectar per http
+        httppool = urllib3.PoolManager()
+        # enviar peticio
+        resposta = httppool.request('GET', self.url)
+        # guardar html resposta
+        self.html = resposta.data.decode('utf-8')
+        
+    
+    def parse_bs4(self):
+        soup = bs4.BeautifulSoup(self.html, features='html.parser')
+        div_main = soup.find_all('div', attrs={'id': 'main'})[0]
+        self.data = div_main.find_all('pre')[0]
+        
+    
+    def extract_data(self):
+        # extreure dades del html
+        self.data = self.data.text
+       
+    
+    def parse_html(self):
+        # parse bs4
+        self.parse_bs4()
+        # extreure dades del html
+        self.extract_data()
+    
+    def get_data(self):
+        # descarregar web
+        self.get_web()
+        # llegir html
+        self.parse_html()
+        # retornar dades
+        return self.data
+    
+
+if __name__== "__main__":
+    client = WebScrape()
+    dades = client.get_data()
+    print(dades)
